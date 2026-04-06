7928713f742cfe6fb5cf4e9194c5e0c9284f4b8d
Rafael Pierangeli <rafael_jose92@hotmail.com>
2026-04-06 09:00:36 -0300

    Fix TnT
    add bonus strengh to lvl
    fix drop block on use tnt

--- src/main/java/com/player_level_skills/mixin/block/AbstractBlockMixin.java
+++ src/main/java/com/player_level_skills/mixin/block/AbstractBlockMixin.java
@@ -1,7 +1,5 @@
 package com.player_level_skills.mixin.block;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.player_level_skills.access.LevelManagerAccess;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.BlockState;
@@ -10,10 +8,11 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.loot.context.LootWorldContext;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 import java.util.function.BiConsumer;
@@ -21,11 +20,12 @@ import java.util.function.BiConsumer;
 @Mixin(AbstractBlock.class)
 public class AbstractBlockMixin {
 
-//    @WrapOperation(method = "onExploded", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getDroppedStacks(Lnet/minecraft/loot/context/LootContextParameterSet$Builder;)Ljava/util/List;"))
-//    private List<ItemStack> onExplodedMixin(BlockState instance, LootWorldContext lootWorldContext, Operation<List<ItemStack>> original, BlockState state, ServerWorld world, BlockPos pos, Explosion explosion, BiConsumer<ItemStack, BlockPos> stackMerger) {
-//        if (explosion.getCausingEntity() instanceof PlayerEntity playerEntity && !playerEntity.isCreative() && !((LevelManagerAccess) playerEntity).getLevelManager().hasRequiredMiningLevel(state.getBlock())) {
-//            return List.of();
-//        }
-//        return original.call(instance, lootWorldContext);
-//    }
+    @Inject(method = "onExploded", at = @At("HEAD"), cancellable = true)
+    private void onExplodedMixin(BlockState state, ServerWorld world, BlockPos pos, Explosion explosion, BiConsumer<ItemStack, BlockPos> stackMerger, CallbackInfo ci) {
+        if (explosion.getCausingEntity() instanceof PlayerEntity playerEntity
+                && !playerEntity.isCreative()
+                && !((LevelManagerAccess) playerEntity).getLevelManager().hasRequiredMiningLevel(state.getBlock())) {
+            ci.cancel();
+        }
+    }
 }
--- src/main/java/com/player_level_skills/mixin/entity/TntEntityMixin.java
+++ src/main/java/com/player_level_skills/mixin/entity/TntEntityMixin.java
@@ -3,10 +3,7 @@ package com.player_level_skills.mixin.entity;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.player_level_skills.util.BonusHelper;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.TntEntity;
+import net.minecraft.entity.*;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
@@ -21,7 +18,7 @@ public abstract class TntEntityMixin extends Entity {
 
     @Shadow
     @Nullable
-    private LivingEntity causingEntity;
+    private LazyEntityReference<LivingEntity> causingEntity;
 
     public TntEntityMixin(EntityType<?> type, World world) {
         super(type, world);
@@ -31,7 +28,10 @@ public abstract class TntEntityMixin extends Entity {
             "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/damage/DamageSource;Lnet/minecraft/world/explosion/ExplosionBehavior;DDDFZLnet/minecraft/world/World$ExplosionSourceType;)V"
     ))
     private void explosionMixin(World instance, Entity entity, DamageSource damageSource, ExplosionBehavior behavior, double x, double y, double z, float power, boolean createFire, World.ExplosionSourceType explosionSourceType, Operation<Void> original) {
-        if (causingEntity != null && causingEntity instanceof PlayerEntity playerEntity) {
+
+        LivingEntity causing = LazyEntityReference.getLivingEntity(causingEntity, this.getEntityWorld());
+
+        if (causing instanceof PlayerEntity playerEntity) {
             power += BonusHelper.tntStrengthBonus(playerEntity);
         }
         original.call(instance, entity, damageSource, behavior, x, y, z, power, createFire, explosionSourceType);
